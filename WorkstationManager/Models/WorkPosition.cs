d1deebd415cb97f523ff561999f451029e6e1ed9
Bruno Jaklić <bruno.jaklic.2005@gmail.com>
2025-07-02 19:02:46 +0200

    Make description fields nullable in models

--- WorkstationManager/Models/WorkPosition.cs
+++ WorkstationManager/Models/WorkPosition.cs
@@ -4,6 +4,6 @@
     {
         public int Id { get; set; }
         public string WorkPositionName { get; set; } = "";
-        public string WorkPositionDescription { get; set; } = "";
+        public string? WorkPositionDescription { get; set; }
     }
 }
