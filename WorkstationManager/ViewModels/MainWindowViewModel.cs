d3a83089d1a285e4a548b37e5181272a0d413e30
Bruno Jaklić <bruno.jaklic.2005@gmail.com>
2025-07-02 17:23:15 +0200

    Initial commit

---
+++ WorkstationManager/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace WorkstationManager.ViewModels
+{
+    public partial class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting { get; } = "Di si Lega!";
+    }
+}
---
+++ WorkstationManager/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace WorkstationManager.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
