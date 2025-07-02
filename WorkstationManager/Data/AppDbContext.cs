f6796f43c43eec128c1f17a4c0b470891de012a6
Bruno Jaklić <bruno.jaklic.2005@gmail.com>
2025-07-02 19:35:28 +0200

    Add AppDbContext with MySQL configuration

---
+++ WorkstationManager/Data/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using WorkstationManager.Models;
+
+namespace WorkstationManager.Data
+{
+    public class AppDbContext : DbContext
+    {
+
+        public DbSet<User> Users { get; set; } = null!;
+        public DbSet<Role> Roles { get; set; } = null!;
+        public DbSet<WorkPosition> WorkPositions { get; set; } = null!;
+        public DbSet<UserWorkPosition> UserWorkPositions { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseMySql(
+                "server=localhost;database=workstation_db;user=root;password=yourpassword;",
+                new MySqlServerVersion(new Version(8, 0, 42))
+            );
+        }
+
+    }
+}
