6c91510cf0f4b99c42ad98d41a6e7321d8f6b22c
Bruno Jaklić <bruno.jaklic.2005@gmail.com>
2025-07-03 00:57:19 +0200

    Add initial EF Core migrations and seed roles

--- WorkstationManager/Data/AppDbContext.cs
+++ WorkstationManager/Data/AppDbContext.cs
@@ -15,8 +15,16 @@ namespace WorkstationManager.Data
         protected override void OnConfiguring(DbContextOptionsBuilder options)
         {
             options.UseMySql(
-                "server=localhost;database=workstation_db;user=root;password=yourpassword;",
+                "server=localhost;database=workstation_db;user=Bruno;password=password123;",
                 new MySqlServerVersion(new Version(8, 0, 42))
+            );
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Role>().HasData(
+                new Role { Id = 1, RoleName = "Admin" },
+                new Role { Id = 2, RoleName = "User" }
             );
         }
 
