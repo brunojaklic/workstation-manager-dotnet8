f5b34e0d35f767d856ce6e3fb8f222e3ab1e6dad
Bruno Jaklić <bruno.jaklic.2005@gmail.com>
2025-07-03 19:53:26 +0200

    Add username and password bindings to sign-in form

--- WorkstationManager/ViewModels/MainWindowViewModel.cs
+++ WorkstationManager/ViewModels/MainWindowViewModel.cs
@@ -1,10 +1,17 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using System;
 
 namespace WorkstationManager.ViewModels
 {
     public partial class MainWindowViewModel : ObservableObject
     {
+        [ObservableProperty]
+        private string username = "";
+
+        [ObservableProperty]
+        private string password = "";
+
         [RelayCommand]
         private void SignIn()
         {
